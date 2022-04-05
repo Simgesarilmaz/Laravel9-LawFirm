cba42c2b64152e74c5614902ab2dc8670d0fc785
Simgesarilmaz <ysarilmaz68@gmail.com>
2022-04-05 12:10:49 +0300

    Admin panel template created

--- resources/views/admin/_content.blade.php
+++ resources/views/admin/_content.blade.php
@@ -1,3 +1,13 @@
+<!-- /. NAV SIDE  -->
+<div id="page-wrapper">
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-head-line">DASHBOARD</h1>
+                <h1 class="page-subhead-line">This is dummy text , you can replace it with your original text. </h1>
+
+            </div>
+        </div>
 <!-- /. ROW  -->
 <div class="row">
     <div class="col-md-4">
